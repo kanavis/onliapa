93f72e71eed16ac881f29a97b3f301448dc80f03
Dmitry Konovalov <kanavis@rinet.ru>
2020-04-19 18:56:54 +0300

    Fix host

--- frontend/environment.prod.ts
+++ frontend/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  ws_create: 'wss://onliapa.kanavis.pw:1288/ws/new_game',
-  ws_admin: 'wss://onliapa.kanavis.pw:1288/ws/admin',
-  ws_game: 'wss://onliapa.kanavis.pw:1288/ws/game',
+  ws_create: 'wss://sh.kanavis.pw:1288/ws/new_game',
+  ws_admin: 'wss://sh.kanavis.pw:1288/ws/admin',
+  ws_game: 'wss://sh.kanavis.pw:1288/ws/game',
 };

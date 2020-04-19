4c086e7d46bc8b49a93f9e0ae37033fe743aaa71
Dmitry Konovalov <kanavis@rinet.ru>
2020-04-19 18:52:06 +0300

    Env fix

---
+++ frontend/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  ws_create: 'wss://onliapa.kanavis.pw:1288/ws/new_game',
+  ws_admin: 'wss://onliapa.kanavis.pw:1288/ws/admin',
+  ws_game: 'wss://onliapa.kanavis.pw:1288/ws/game',
+};
---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  ws_create: 'ws://localhost:6613/ws/new_game',
+  ws_admin: 'ws://localhost:6613/ws/admin',
+  ws_game: 'ws://localhost:6613/ws/game',
+};
